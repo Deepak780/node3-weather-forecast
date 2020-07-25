6b823792d46d6790c4cd4e296e6a1675d92cc05a
DR <deepukandiboyina@gmail.com>
2020-07-25 13:14:59 +0530

    Removed unnecessary console.log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client Scipt...');
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#location')
