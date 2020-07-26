ac5dcbb21ff8482d6e5ddfe6a66615527933702d
DR <deepukandiboyina@gmail.com>
2020-07-26 23:05:32 +0530

    Error changes

--- src/app.js
+++ src/app.js
@@ -104,6 +104,6 @@ app.get('*', (req,res) => {
 
 // It runs the server by listening to the port
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(port, () => {
+    console.log('Server is running on port ' + port)
 })
